eab70a1ee1f471754876b77c8acaf8e21c7d9805
porihuru <hirohuru25@gmail.com>
2025-12-22 06:55:23 +0900

    Update sp_base.js

--- js/sp_base.js
+++ js/sp_base.js
@@ -1,33 +1,29 @@
-// JST: 2025-12-19 06:37:29 / sp_base.js
+// sp_base.js / 作成日時(JST): 2025-12-22 14:10:00
 (function (global) {
   "use strict";
 
-  var SP_BASE = {
-    webRoot: "",
-    api: ""
-  };
+  function getPathname() {
+    try { return global.location.pathname || ""; } catch (e) { return ""; }
+  }
 
-  function detectWebRoot(pathname) {
-    // 例: /na/.../syunin/DocLib/text_access/index.html
-    // -> /na/.../syunin
-    var p = pathname || "/";
-    var markers = ["/DocLib/", "/Shared%20Documents/", "/Shared Documents/", "/Documents/"];
-    for (var i = 0; i < markers.length; i++) {
-      var m = markers[i];
-      var idx = p.indexOf(m);
-      if (idx >= 0) return p.substring(0, idx);
-    }
-    // fallback: 最終 / を切る（精度は落ちる）
-    var last = p.lastIndexOf("/");
-    if (last > 0) return p.substring(0, last);
+  // /.../DocLib/text_access/index.html から、/DocLib より前を webRoot として採用
+  // 見つからない場合は「現在ディレクトリの1つ上」を仮採用（環境で調整）
+  function detectWebRoot() {
+    var path = getPathname();
+    var idx = path.indexOf("/DocLib/");
+    if (idx !== -1) return path.substring(0, idx);
+
+    // fallback: /a/b/c/index.html -> /a/b/c
+    var last = path.lastIndexOf("/");
+    if (last > 0) return path.substring(0, last);
     return "";
   }
 
-  SP_BASE.init = function () {
-    SP_BASE.webRoot = detectWebRoot(window.location.pathname);
-    SP_BASE.api = SP_BASE.webRoot + "/_api";
+  var webRoot = detectWebRoot();
+  var api = webRoot + "/_api";
+
+  global.SP_BASE = {
+    webRoot: webRoot,
+    api: api
   };
-
-  global.SP_BASE = SP_BASE;
-
 })(window);
