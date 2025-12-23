6d08700bb474afcff43fceb1a543b9a15111fbb0
porihuru <hirohuru25@gmail.com>
2025-12-23 10:21:07 +0900

    sp_base.js を更新

--- js/sp_base.js
+++ js/sp_base.js
@@ -1,4 +1,4 @@
-// sp_base.js / 作成日時(JST): 2025-12-22 14:10:00
+// sp_base.js / 作成日時(JST): 2025-12-23 10:30:00
 (function (global) {
   "use strict";
 
@@ -6,21 +6,39 @@
     try { return global.location.pathname || ""; } catch (e) { return ""; }
   }
 
-  // /.../DocLib/text_access/index.html から、/DocLib より前を webRoot として採用
-  // 見つからない場合は「現在ディレクトリの1つ上」を仮採用（環境で調整）
+  function trimEndSlash(s) {
+    if (!s) return "";
+    while (s.length > 1 && s.charAt(s.length - 1) === "/") {
+      s = s.substring(0, s.length - 1);
+    }
+    return s;
+  }
+
+  // /.../DocLib/ や /.../DocLib1/ の「/DocLib(数字)/」より前を webRoot とする
   function detectWebRoot() {
     var path = getPathname();
-    var idx = path.indexOf("/DocLib/");
-    if (idx !== -1) return path.substring(0, idx);
 
-    // fallback: /a/b/c/index.html -> /a/b/c
+    // DocLib, DocLib1, DocLib2 ... を想定
+    var re = /\/DocLib\d*\//i;
+    var m = re.exec(path);
+    if (m && typeof m.index === "number" && m.index >= 0) {
+      return path.substring(0, m.index);
+    }
+
+    // 互換：DocLib を含むが末尾に / が無い等のケース
+    var idx2 = path.toLowerCase().indexOf("/doclib");
+    if (idx2 !== -1) {
+      return path.substring(0, idx2);
+    }
+
+    // 最終手段：現在ディレクトリ（＝ここは環境により不正になることがある）
     var last = path.lastIndexOf("/");
     if (last > 0) return path.substring(0, last);
     return "";
   }
 
-  var webRoot = detectWebRoot();
-  var api = webRoot + "/_api";
+  var webRoot = trimEndSlash(detectWebRoot());
+  var api = webRoot ? (webRoot + "/_api") : "/_api";
 
   global.SP_BASE = {
     webRoot: webRoot,
